d0c317299c51bef021f674c43c2c1a16b74b81aa
eedsuaidi <eedsuaidi.ms@gmail.com>
2022-11-08 09:39:47 +0700

    Video 10 End

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -17,19 +17,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
 
-        User::create([
-            'name' => 'Eed Suaidi',
-            'email' => 'eed@gmail.com',
-            'password' => bcrypt('12345')
-        ]);
+        // User::create([
+        //     'name' => 'Eed Suaidi',
+        //     'email' => 'eed@gmail.com',
+        //     'password' => bcrypt('12345')
+        // ]);
 
-        User::create([
-            'name' => 'Jennie',
-            'email' => 'jennie@gmail.com',
-            'password' => bcrypt('12345')
-        ]);
+        // User::create([
+        //     'name' => 'Jennie',
+        //     'email' => 'jennie@gmail.com',
+        //     'password' => bcrypt('12345')
+        // ]);
+
+        User::factory(3)->create();
 
         Category::create([
             'name' => 'Web Programming',
@@ -41,41 +42,43 @@ class DatabaseSeeder extends Seeder
             'slug' => 'personal'
         ]);
 
-        Post::create([
-            'title' => 'Judul Pertama',
-            'slug' => 'judul-pertama',
-            'excerpt' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab inventore eveniet cumque labore eum, dolorem voluptatem quas voluptatibus culpa earum laborum.',
-            'body' => '<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab inventore eveniet cumque labore eum, dolorem voluptatem quas voluptatibus culpa earum laborum. Nisi quam esse explicabo? Veniam tenetur facilis possimus odio provident minus commodi voluptatibus maiores hic deserunt.</p><p>Possimus, necessitatibus eos! Commodi ab vel voluptas veritatis ipsam ea voluptatum, ut tenetur nemo! Natus tempora, beatae optio error delectus, repellat odit nisi rerum facere maxime, odio enim temporibus minus autem! Sit tempora esse dolore repellendus laboriosam reiciendis molestias, iure dolorum id sint nemo incidunt.</p><p>Suscipit unde dolorem, in explicabo modi cumque recusandae consequatur qui reiciendis enim? Quasi ex magni placeat unde numquam maiores ea aliquid recusandae qui tempora error libero molestias earum quidem, nemo at. Alias omnis qui consequatur? Quam facere mollitia vel accusamus cum quia porro fuga? Corporis veniam labore quidem ex delectus saepe! Molestias a quasi ab voluptatem error, accusantium provident odio earum nisi repellat minima id amet numquam possimus.</p>',
-            'category_id' => '1',
-            'user_id' => '1'
-        ]);
+        Post::factory(20)->create();
 
-        Post::create([
-            'title' => 'Judul Ke Dua',
-            'slug' => 'judul-ke-dua',
-            'excerpt' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab inventore eveniet cumque labore eum, dolorem voluptatem quas voluptatibus culpa earum laborum.',
-            'body' => '<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab inventore eveniet cumque labore eum, dolorem voluptatem quas voluptatibus culpa earum laborum. Nisi quam esse explicabo? Veniam tenetur facilis possimus odio provident minus commodi voluptatibus maiores hic deserunt.</p><p>Possimus, necessitatibus eos! Commodi ab vel voluptas veritatis ipsam ea voluptatum, ut tenetur nemo! Natus tempora, beatae optio error delectus, repellat odit nisi rerum facere maxime, odio enim temporibus minus autem! Sit tempora esse dolore repellendus laboriosam reiciendis molestias, iure dolorum id sint nemo incidunt.</p><p>Suscipit unde dolorem, in explicabo modi cumque recusandae consequatur qui reiciendis enim? Quasi ex magni placeat unde numquam maiores ea aliquid recusandae qui tempora error libero molestias earum quidem, nemo at. Alias omnis qui consequatur? Quam facere mollitia vel accusamus cum quia porro fuga? Corporis veniam labore quidem ex delectus saepe! Molestias a quasi ab voluptatem error, accusantium provident odio earum nisi repellat minima id amet numquam possimus.</p>',
-            'category_id' => '1',
-            'user_id' => '1'
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Pertama',
+        //     'slug' => 'judul-pertama',
+        //     'excerpt' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab inventore eveniet cumque labore eum, dolorem voluptatem quas voluptatibus culpa earum laborum.',
+        //     'body' => '<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab inventore eveniet cumque labore eum, dolorem voluptatem quas voluptatibus culpa earum laborum. Nisi quam esse explicabo? Veniam tenetur facilis possimus odio provident minus commodi voluptatibus maiores hic deserunt.</p><p>Possimus, necessitatibus eos! Commodi ab vel voluptas veritatis ipsam ea voluptatum, ut tenetur nemo! Natus tempora, beatae optio error delectus, repellat odit nisi rerum facere maxime, odio enim temporibus minus autem! Sit tempora esse dolore repellendus laboriosam reiciendis molestias, iure dolorum id sint nemo incidunt.</p><p>Suscipit unde dolorem, in explicabo modi cumque recusandae consequatur qui reiciendis enim? Quasi ex magni placeat unde numquam maiores ea aliquid recusandae qui tempora error libero molestias earum quidem, nemo at. Alias omnis qui consequatur? Quam facere mollitia vel accusamus cum quia porro fuga? Corporis veniam labore quidem ex delectus saepe! Molestias a quasi ab voluptatem error, accusantium provident odio earum nisi repellat minima id amet numquam possimus.</p>',
+        //     'category_id' => '1',
+        //     'user_id' => '1'
+        // ]);
 
-        Post::create([
-            'title' => 'Judul Ke Tiga',
-            'slug' => 'judul-ke-tiga',
-            'excerpt' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab inventore eveniet cumque labore eum, dolorem voluptatem quas voluptatibus culpa earum laborum.',
-            'body' => '<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab inventore eveniet cumque labore eum, dolorem voluptatem quas voluptatibus culpa earum laborum. Nisi quam esse explicabo? Veniam tenetur facilis possimus odio provident minus commodi voluptatibus maiores hic deserunt.</p><p>Possimus, necessitatibus eos! Commodi ab vel voluptas veritatis ipsam ea voluptatum, ut tenetur nemo! Natus tempora, beatae optio error delectus, repellat odit nisi rerum facere maxime, odio enim temporibus minus autem! Sit tempora esse dolore repellendus laboriosam reiciendis molestias, iure dolorum id sint nemo incidunt.</p><p>Suscipit unde dolorem, in explicabo modi cumque recusandae consequatur qui reiciendis enim? Quasi ex magni placeat unde numquam maiores ea aliquid recusandae qui tempora error libero molestias earum quidem, nemo at. Alias omnis qui consequatur? Quam facere mollitia vel accusamus cum quia porro fuga? Corporis veniam labore quidem ex delectus saepe! Molestias a quasi ab voluptatem error, accusantium provident odio earum nisi repellat minima id amet numquam possimus.</p>',
-            'category_id' => '2',
-            'user_id' => '1'
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Ke Dua',
+        //     'slug' => 'judul-ke-dua',
+        //     'excerpt' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab inventore eveniet cumque labore eum, dolorem voluptatem quas voluptatibus culpa earum laborum.',
+        //     'body' => '<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab inventore eveniet cumque labore eum, dolorem voluptatem quas voluptatibus culpa earum laborum. Nisi quam esse explicabo? Veniam tenetur facilis possimus odio provident minus commodi voluptatibus maiores hic deserunt.</p><p>Possimus, necessitatibus eos! Commodi ab vel voluptas veritatis ipsam ea voluptatum, ut tenetur nemo! Natus tempora, beatae optio error delectus, repellat odit nisi rerum facere maxime, odio enim temporibus minus autem! Sit tempora esse dolore repellendus laboriosam reiciendis molestias, iure dolorum id sint nemo incidunt.</p><p>Suscipit unde dolorem, in explicabo modi cumque recusandae consequatur qui reiciendis enim? Quasi ex magni placeat unde numquam maiores ea aliquid recusandae qui tempora error libero molestias earum quidem, nemo at. Alias omnis qui consequatur? Quam facere mollitia vel accusamus cum quia porro fuga? Corporis veniam labore quidem ex delectus saepe! Molestias a quasi ab voluptatem error, accusantium provident odio earum nisi repellat minima id amet numquam possimus.</p>',
+        //     'category_id' => '1',
+        //     'user_id' => '1'
+        // ]);
 
-        Post::create([
-            'title' => 'Judul Ke Empat',
-            'slug' => 'judul-ke-empat',
-            'excerpt' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab inventore eveniet cumque labore eum, dolorem voluptatem quas voluptatibus culpa earum laborum.',
-            'body' => '<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab inventore eveniet cumque labore eum, dolorem voluptatem quas voluptatibus culpa earum laborum. Nisi quam esse explicabo? Veniam tenetur facilis possimus odio provident minus commodi voluptatibus maiores hic deserunt.</p><p>Possimus, necessitatibus eos! Commodi ab vel voluptas veritatis ipsam ea voluptatum, ut tenetur nemo! Natus tempora, beatae optio error delectus, repellat odit nisi rerum facere maxime, odio enim temporibus minus autem! Sit tempora esse dolore repellendus laboriosam reiciendis molestias, iure dolorum id sint nemo incidunt.</p><p>Suscipit unde dolorem, in explicabo modi cumque recusandae consequatur qui reiciendis enim? Quasi ex magni placeat unde numquam maiores ea aliquid recusandae qui tempora error libero molestias earum quidem, nemo at. Alias omnis qui consequatur? Quam facere mollitia vel accusamus cum quia porro fuga? Corporis veniam labore quidem ex delectus saepe! Molestias a quasi ab voluptatem error, accusantium provident odio earum nisi repellat minima id amet numquam possimus.</p>',
-            'category_id' => '2',
-            'user_id' => '2'
-        ]);
+        // Post::create([
+        //     'title' => 'Judul Ke Tiga',
+        //     'slug' => 'judul-ke-tiga',
+        //     'excerpt' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab inventore eveniet cumque labore eum, dolorem voluptatem quas voluptatibus culpa earum laborum.',
+        //     'body' => '<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab inventore eveniet cumque labore eum, dolorem voluptatem quas voluptatibus culpa earum laborum. Nisi quam esse explicabo? Veniam tenetur facilis possimus odio provident minus commodi voluptatibus maiores hic deserunt.</p><p>Possimus, necessitatibus eos! Commodi ab vel voluptas veritatis ipsam ea voluptatum, ut tenetur nemo! Natus tempora, beatae optio error delectus, repellat odit nisi rerum facere maxime, odio enim temporibus minus autem! Sit tempora esse dolore repellendus laboriosam reiciendis molestias, iure dolorum id sint nemo incidunt.</p><p>Suscipit unde dolorem, in explicabo modi cumque recusandae consequatur qui reiciendis enim? Quasi ex magni placeat unde numquam maiores ea aliquid recusandae qui tempora error libero molestias earum quidem, nemo at. Alias omnis qui consequatur? Quam facere mollitia vel accusamus cum quia porro fuga? Corporis veniam labore quidem ex delectus saepe! Molestias a quasi ab voluptatem error, accusantium provident odio earum nisi repellat minima id amet numquam possimus.</p>',
+        //     'category_id' => '2',
+        //     'user_id' => '1'
+        // ]);
+
+        // Post::create([
+        //     'title' => 'Judul Ke Empat',
+        //     'slug' => 'judul-ke-empat',
+        //     'excerpt' => 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab inventore eveniet cumque labore eum, dolorem voluptatem quas voluptatibus culpa earum laborum.',
+        //     'body' => '<p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ab inventore eveniet cumque labore eum, dolorem voluptatem quas voluptatibus culpa earum laborum. Nisi quam esse explicabo? Veniam tenetur facilis possimus odio provident minus commodi voluptatibus maiores hic deserunt.</p><p>Possimus, necessitatibus eos! Commodi ab vel voluptas veritatis ipsam ea voluptatum, ut tenetur nemo! Natus tempora, beatae optio error delectus, repellat odit nisi rerum facere maxime, odio enim temporibus minus autem! Sit tempora esse dolore repellendus laboriosam reiciendis molestias, iure dolorum id sint nemo incidunt.</p><p>Suscipit unde dolorem, in explicabo modi cumque recusandae consequatur qui reiciendis enim? Quasi ex magni placeat unde numquam maiores ea aliquid recusandae qui tempora error libero molestias earum quidem, nemo at. Alias omnis qui consequatur? Quam facere mollitia vel accusamus cum quia porro fuga? Corporis veniam labore quidem ex delectus saepe! Molestias a quasi ab voluptatem error, accusantium provident odio earum nisi repellat minima id amet numquam possimus.</p>',
+        //     'category_id' => '2',
+        //     'user_id' => '2'
+        // ]);
 
     }
 }
--- resources/views/category.blade.php
+++ resources/views/category.blade.php
@@ -6,9 +6,11 @@
     @foreach ($posts as $post)
         <article class="mb-5">
             <h2>
-                <a href="/posts/{{ $post->slug }}">{{ $post->title }}</a>
+                <a href="/posts/{{ $post->slug }}" class="text-decoration-none">{{ $post->title }}</a>
             </h2>
-            <h5>By: {{ $post->author }}</h5>
+            
+            <p>By. <a href="/authors/{{ $post->author->username }}" class="text-decoration-none">{{ $post->author->name }}</a> in <a href="/categories/{{ $post->category->slug }}" class="text-decoration-none">{{ $post->category->name }}</a></p>
+
             <p>{{ $post->excerpt }}</p>
         </article>
     @endforeach
